905ac4171c4da9d402caaa98ed843664c92b8d53
Victor Moraes <victor.moraes18.8.75@gmail.com>
2026-02-06 15:24:10 -0300

    Desafios básicos de JS.

---
+++ atividades/desafio_area.js
@@ -0,0 +1,6 @@
+
+const pi = 3.14159;
+const raio = 10;
+
+console.log("Considerando que o valor de PI é " + pi + " e o valor do raio é " + raio + "...")
+console.log("A área da circunferência é de: " + pi * raio * raio + "m²");
---
+++ atividades/desafio_troca.js
@@ -0,0 +1,12 @@
+let a = 7;
+let b = 94;
+
+console.log("O valor A é: " + a);
+console.log("O valor B é: " + b);
+console.log("\nRealizando troca... \n");
+
+a = 94;
+b = 7;
+
+console.log("O novo valor A é: " + a);
+console.log("O novo valor B é: " + b);
