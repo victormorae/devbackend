8e72b7562ab6c1ecf02e1b61d8b704034db2650f
Victor Moraes <victor.moraes18.8.75@gmail.com>
2026-03-06 18:02:40 -0300

    Randomizador de atributos adicionado.

---
+++ desafios/numeros_aleatorios.js
@@ -0,0 +1,18 @@
+const forca = Math.floor(Math.random() * 112);
+const vigor = Math.floor(Math.random() * 112);
+const destreza = Math.floor(Math.random() * 112);
+const agilidade = Math.floor(Math.random() * 112);
+const luta = Math.floor(Math.random() * 112);
+const intelecto = Math.floor(Math.random() * 112);
+const prontidao = Math.floor(Math.random() * 112);
+const presenca = Math.floor(Math.random() * 112);
+
+console.log(`Atributos:
+**Força**: ${forca} 
+**Vigor**: ${vigor} 
+**Destreza**: ${destreza}
+**Agilidade**: ${agilidade} 
+**Luta**: ${luta}
+**Intelecto**: ${intelecto}
+**Prontidão**: ${prontidao}
+**Presença**: ${presenca}`);
